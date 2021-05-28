4822b037c693404c4edc2d8316abd4cb66787817
MatteaAnn Swain <MMSwain@tql.com>
2021-05-28 15:55:24 -0400

    Add project files.

---
+++ EFSalesDb/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFSalesDb.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderLines = new HashSet<OrderLine>();
+        }
+
+        public int Id { get; set; }
+        public int? CustomerId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+// virtual to properties means this property is not in the db but in the class
+        public virtual Customer Customer { get; set; }
+        public virtual ICollection<OrderLine> OrderLines { get; set; }
+    }
+}
---
+++ EFSalesDb/Program.cs
@@ -0,0 +1,45 @@
+﻿using EFSalesDb.Models;
+using System;
+using System.Linq;
+
+namespace EFSalesDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new SalesdbContext(); // insert instance of your dbcontextclass
+
+            var customers = context.Customers.ToList(); // gets all customer from db
+
+            var customer = context.Customers.Find(1); // get customer by PK 
+
+
+            var customersInCinci = context.Customers // get customer that are located in city Cinci
+                .Where(c => c.City == "Cincinnati")
+                .ToList(); // return data in list
+
+// INSERT NEW CUSTOMER
+            var newCustomer = new Customer()
+            {
+                Name = "TQL", City = "Cincinnati", State = "OH", Sales = 4000000, Active = true
+            };
+
+            //context.Customers.Add(newCustomer); // 
+            //var rowsAffected = context.SaveChanges(); // saveschanges actually commits the actions
+
+            // UPDATING BY PK
+            var tql = context.Customers.Find(36); // read the record
+            tql.Name = "Total Quality Logistics"; // what changes you want 
+
+            var rowsAffected = context.SaveChanges();// return 
+
+            tql = context.Customers.Find(36); // read the data
+            context.Customers.Remove(tql); // deletes the row (what you want to delete goes in here) 
+            rowsAffected = context.SaveChanges(); // returns rows affected and saves changes 
+        }
+
+
+
+    }
+}
